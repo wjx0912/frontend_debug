7812886ea2ad67dcd0c221c877a6b7a6186746c5
wyt <80101277@qq.com>
2023-08-25 09:12:03 +0800

    add debug

--- vue3_vite_js/vite.config.js
+++ vue3_vite_js/vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false,
+    minify: false
+  },
   plugins: [vue()],
 })
