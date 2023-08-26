35784c5140d63aea6a0f9e223eaaba05a3d74c6c
wyt <80101277@qq.com>
2023-08-26 14:04:33 +0800

    enable build sourcemap

--- vue3_vite_js/vite.config.js
+++ vue3_vite_js/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: false,
+    sourcemap: true,
     minify: false
   },
   plugins: [vue()],
